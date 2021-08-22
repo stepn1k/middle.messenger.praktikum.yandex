4ae1fa5d64e436f79082454cec712720f5081ceb
Stepan <stepn1k@yandex.by>
2021-08-22 12:20:02 +0300

    fix styles issues

--- src/pages/messenger/messenger.template.js
+++ src/pages/messenger/messenger.template.js
@@ -3,22 +3,31 @@ export default `
     <!-- List -->
     <div class="messenger-list">
         <div class="messenger-list-header">
-            <a href="/profile" class="messenger-list-header__profile">Profile ></a>
+            <!--  Profile -->
+            <a class="messenger-list-header__profile" href="/profile">Profile &#10148;</a>
+            <!-- Quick Search -->
             <input class="messenger-list-header__search" placeholder="Quick Search...">
         </div>
         <divider-component />
+        <!-- Chats -->
         <div class="messenger-list-body">
-        
           <FOR_STRUCTURE ARRAY="chats">
+            <!-- Chat -->
             <div class="messenger-list-body-chat">
-                <div class="messenger-list-body-chat__username">{{ chats.INDEX.name }}</div>
-                <div class="messenger-list-body-chat__last-message">{{ chats.INDEX.message }}</div>
+                <!-- Avatar -->
+                <div class="messenger-list-body-chat-avatar"></div>
+                <div class="messenger-list-body-chat-content">
+                     <!--  Username  -->
+                    <div class="messenger-list-body-chat-content__username">{{ chats.INDEX.name }}</div>
+                     <!--  Message  -->
+                    <div class="messenger-list-body-chat-content__last-message">{{ chats.INDEX.message }}</div>
+                </div>
             </div>
             <divider-component />
           </FOR_STRUCTURE>
-          
-        </div>
+         </div>
     </div>
+    
     <!-- Chat -->
     <div class="messenger-chat">
         <!--  Chat Header  -->
@@ -33,7 +42,7 @@ export default `
         <divider-component />
         <!--  Chat Footer  -->
         <div class="messenger-chat-footer">
-            <input placeholder="Message..." class="messenger-chat-footer__input">
+            <input placeholder="Message..." class="messenger-chat-footer__input" name="message" id="message" type="text">
             <div class="messenger-chat-footer__send"></div>
          </div>
     </div>
