e58a0f6db0a4dac57f4b024b4a424b46abb33ef2
Stepan <stepn1k@yandex.by>
2021-10-09 23:53:04 +0300

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const loader = require('ts-loader');
 
 module.exports = {
   mode: 'development',
