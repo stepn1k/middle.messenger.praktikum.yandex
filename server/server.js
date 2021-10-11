c33b1c0afde3686e463129767e6d21345d00da5f
Stepan <stepn1k@yandex.by>
2021-10-11 09:22:48 +0300

    fix express server

--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const distPath = `${__dirname}/../dist`;
 
-app.use(express.static(`${__dirname}/dist`));
-
-app.all('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
+app.use(express.static(distPath));
+app.use((req, res) => {
+  res.sendFile(path.resolve(`${distPath}/index.html`));
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port  ${port}!`);
+  console.log(`App listening on port ${port}!`);
 });
